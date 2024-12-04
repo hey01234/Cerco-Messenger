4ee65bbcafe7bb2076f375aeed2f954f2d244143
hey01234 <hey0000x@gmail.com>
2024-12-04 10:13:12 +0100

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // script.js
 document.querySelectorAll('.transition-link').forEach(link => {
     link.addEventListener('click', function(e) {
+        e.preventDefault();  // Empêcher la redirection immédiate
+
+        // Ajouter la classe slide-out-right pour la page actuelle
         document.body.classList.add('slide-out-right');
         
+        // Récupérer l'URL du lien
+        const targetUrl = link.getAttribute('href');
+
+        // Après que l'animation se soit terminée (1 seconde), rediriger vers la nouvelle page
         setTimeout(() => {
-            window.location.href = e.target.href;
-        }, 1000);
-        e.preventDefault();
+            window.location.href = targetUrl;  // Rediriger vers l'URL récupérée
+        }, 1000); // Attendre que l'animation dure 1 seconde
     });
 });
 // Récupérer le bouton de basculement du thème
