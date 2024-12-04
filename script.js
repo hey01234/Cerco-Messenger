bb7b1c33a00c7bedb86c021356fcef95a915a0ba
hey01234 <hey0000x@gmail.com>
2024-12-04 15:49:19 +0100

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-function toggleMode() {
+function toggleTheme() {
     const body = document.body;
-    const button = document.getElementById('modeButton');
-    const icon = document.getElementById('modeIcon');
-    if (body.classList.contains('bg-gray-100')) {
-        body.classList.remove('bg-gray-100', 'text-gray-800');
-        body.classList.add('bg-gray-800', 'text-gray-100');
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-        button.title = 'Mode clair';
+    const themeIcon = document.getElementById('theme-icon');
+    
+    if (body.classList.contains('theme-light')) {
+        body.classList.remove('theme-light');
+        body.classList.add('theme-dark');
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
     } else {
-        body.classList.remove('bg-gray-800', 'text-gray-100');
-        body.classList.add('bg-gray-100', 'text-gray-800');
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
-        button.title = 'Mode sombre';
+        body.classList.remove('theme-dark');
+        body.classList.add('theme-light');
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
     }
 }
 
 function showLoginForm() {
-    document.getElementById('homePage').classList.add('hidden');
-    document.getElementById('loginForm').classList.remove('hidden');
-    document.getElementById('registerForm').classList.add('hidden');
+    document.getElementById('homepage').classList.add('hidden');
+    document.getElementById('login-section').classList.remove('hidden');
+    document.getElementById('register-section').classList.add('hidden');
 }
 
 function showRegisterForm() {
-    document.getElementById('homePage').classList.add('hidden');
-    document.getElementById('loginForm').classList.add('hidden');
-    document.getElementById('registerForm').classList.remove('hidden');
+    document.getElementById('homepage').classList.add('hidden');
+    document.getElementById('login-section').classList.add('hidden');
+    document.getElementById('register-section').classList.remove('hidden');
 }
 
 function goBack() {
-    document.getElementById('homePage').classList.remove('hidden');
-    document.getElementById('loginForm').classList.add('hidden');
-    document.getElementById('registerForm').classList.add('hidden');
+    document.getElementById('homepage').classList.remove('hidden');
+    document.getElementById('login-section').classList.add('hidden');
+    document.getElementById('register-section').classList.add('hidden');
 }
