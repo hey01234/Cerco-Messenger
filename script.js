1b7726e659eb8acc60a53793725a8b6801569a25
hey01234 <hey0000x@gmail.com>
2024-12-04 09:25:08 +0100

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // script.js
+document.getElementById('theme-switch').addEventListener('change', function () {
+    const body = document.body;
+    const themeText = document.getElementById('theme-text');
+
+    if (this.checked) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        themeText.textContent = 'Mode clair';
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        themeText.textContent = 'Mode sombre';
+    }
+});
+
 document.getElementById('login-btn').addEventListener('click', function () {
     document.getElementById('signup-form').classList.add('hidden');
     document.getElementById('login-form').classList.remove('hidden');
