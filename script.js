a97866f8f3e15f39cedf19e68ce87c44115c22df
hey01234 <hey0000x@gmail.com>
2024-12-05 21:27:41 +0100

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function toggleTheme() {
     const body = document.body;
     const themeIcon = document.getElementById('theme-icon');
+    const logo = document.querySelector('.logo') 
         
     if (body.classList.contains('theme-light')) {
         body.classList.remove('theme-light');
         body.classList.add('theme-dark');
         themeIcon.classList.remove('fa-moon');
         themeIcon.classList.add('fa-sun');
+        logo.src = 'logo-dark.png'
         
     } else {
         body.classList.remove('theme-dark');
         body.classList.add('theme-light');
         themeIcon.classList.remove('fa-sun');
         themeIcon.classList.add('fa-moon');
+        logo.src = 'logo.png'
     }
 }
 
@@ -33,3 +36,44 @@ function goBack() {
     document.getElementById('login-section').classList.add('hidden');
     document.getElementById('register-section').classList.add('hidden');
 }
+
+
+  // Gérer l'inscription
+  document.querySelector("#register-section .primary-button").addEventListener("click", async () => {
+    const idUtilisateur = document.getElementById("register-username").value;
+    const motDePasse = document.getElementById("register-password").value;
+    const confirmationMotDePasse = document.getElementById("confirm-password").value;
+
+    try {
+      const response = await fetch("http://localhost:3000/inscription", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ idUtilisateur, motDePasse, confirmationMotDePasse }),
+      });
+
+      const data = await response.json();
+      alert(data.message);
+    } catch (error) {
+      alert("Erreur lors de l'inscription : " + error.message);
+    }
+  });
+
+  // Gérer la connexion
+  document.querySelector("#login-section .primary-button").addEventListener("click", async () => {
+    const idUtilisateur = document.getElementById("login-username").value;
+    const motDePasse = document.getElementById("login-password").value;
+
+    try {
+      const response = await fetch("http://localhost:3000/connexion", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ idUtilisateur, motDePasse }),
+      });
+
+      const data = await response.json();
+      alert(data.message);
+    } catch (error) {
+      alert("Erreur lors de la connexion : " + error.message);
+    }
+  });
+
